b3dff8ba568f290e61bcb5d4039236e542bec1a4
Chris_Primavera <Chris_Primavera@aspensquare.com>
2016-08-02 15:39:55 -0400

    bare bones express configuration

--- app.js
+++ app.js
@@ -1,64 +1,44 @@
 var express = require( 'express' );
+var expressValidator = require( 'express-validator' );
 var path = require( 'path' );
-var config = require( 'config' );
-var mongoose = require( 'mongoose' );
-var logger = require( 'morgan' );
+var favicon = require( 'serve-favicon' );
 var cookieParser = require( 'cookie-parser' );
 var bodyParser = require( 'body-parser' );
-var routes = require( './routes/index' );
+var config = require( 'config' );
 var app = express();
-//var conn = config.get( 'data.leasing.connection' );
-
-//mongoose.connect( conn );
-//cache.connect( config.get( 'redis' ) );
 
 // view engine setup
 app.set( 'views', path.join( __dirname, 'views' ) );
 app.set( 'view engine', 'hbs' );
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use( logger( 'dev' ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( {extended: false} ) );
 app.use( cookieParser() );
-app.use( require( 'less-middleware' )( path.join( __dirname, 'public' ) ) );
 app.use( express.static( path.join( __dirname, 'public' ) ) );
+app.use( expressValidator() );
 
-//app.use( function ( req, res, next ) {
-//    sanitizer.sanitizeRequestBody( req );
-//    sanitizer.sanitizeRequestQueryString( req );
-//    next();
-//} );
-
-// Add Cross Domain Authorization
-// TODO - REMOVE Origin '*'
 app.use( function ( req, res, next ) {
     res.setHeader( 'Access-Control-Allow-Origin', '*' );
     res.setHeader( 'Access-Control-Allow-Headers', 'content-type, Authorization' );
     res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE' );
-    res.setHeader( 'Access-Control-Expose-Headers', 'Location' );
     next();
 } );
 
-app.use( '/', routes );
-
-// error handlers
-// development error handler
-// will print stacktrace
-//if ( app.get( 'env' ) === 'development' ) {
-if ( 1 === 1 ) {
+// ERROR HANDLERS
+// development error handler, will print stacktrace
+if ( app.get( 'env' ) === 'development' ) {
     app.use( function ( err, req, res, next ) {
-        var status = err.status || 500;
-        res.status( status ).send( {status: status, message: err.message, stack: err} );
+        res.status( err.status || 500 );
+
+        res.setHeader( 'Content-Type', 'application/json' );
+        res.send( JSON.stringify( {message: err.message, stack: err.stack} ) );
     } );
 }
-
-// production error handler
-// no stacktraces leaked to user
+// production error handler, no stacktraces leaked to user
 app.use( function ( err, req, res, next ) {
-    var status = err.status || 500;
-    res.status( status ).send( {status: status, message: err.message} );
+    res.status( err.status || 500 );
+    res.setHeader( 'Content-Type', 'application/json' );
+    res.send( JSON.stringify( {message: err.message} ) );
 } );
 
 module.exports = app;
